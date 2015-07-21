9ce4e4809095c60453cd1bfd24ede217123480be
ilix <me@ilix.se>
2015-07-21 23:11:16 +0200

    Moving to docker-compose.

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 docker build -t ingr-api .
-docker tag -f ingr-api tutum.co/alexanderczigler/ingr-api
-docker push tutum.co/alexanderczigler/ingr-api
+docker tag -f ingr-api tutum.co/ilix/ingr-api
+docker push tutum.co/ilix/ingr-api
