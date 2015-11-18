7e3b520857648e34dde46ff6ca0814132916a5ab
ilix <me@ilix.se>
2015-11-18 20:08:13 +0100

    Code cleanup.

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
+'use strict'
 
-
-
-/*
- * Instagrannar API.
- */
 var http = require('http')
 var express = require('express')
 var pictures = require('./lib/routes/pictures.js')
@@ -11,11 +7,6 @@ var pictures = require('./lib/routes/pictures.js')
 var app = express()
 app.set('port', 3000)
 
-
-
-/*
- * Enable CORS.
- */
 app.all('/*', function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*')
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
@@ -26,19 +17,9 @@ app.options(/(.*)/, function (req, res, next) {
   res.send(200) // Always respond OK on OPTIONS requests.
 })
 
-
-
-/*
- * Routes.
- */
-app.get('/pictures/:id/:accesstoken', pictures.byId)
 app.get('/pictures', pictures.byLocation)
+app.get('/pictures/:id/:accesstoken', pictures.byId)
 
-
-
-/*
- * Start.
- */
 http.createServer(app).listen(app.get('port'), function () {
-  console.log('ingr api listening on port ' + app.get('port'))
+  console.log('Instagrannar API listening on port ' + app.get('port'))
 })
