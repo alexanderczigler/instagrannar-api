9bece18b427c227f87e356fe8bd1d729632d4e45
ilix <me@ilix.se>
2015-11-18 19:26:30 +0100

    docker stuff

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 docker build -t ingr-api .
-docker tag -f ingr-api tutum.co/ilix/ingr-api
-docker push tutum.co/ilix/ingr-api
+docker tag -f ingr-api tutum.co/instagrannar/ingr-api
+docker push tutum.co/instagrannar/ingr-api
